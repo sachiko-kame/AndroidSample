b5cbb2eb5d7f09525ea801196f290901bd3285da
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-05 22:09:37 +0900

    テーブルの表示を2行に

--- app/src/main/java/com/example/kameko/myapplication/Main3Activity.kt
+++ app/src/main/java/com/example/kameko/myapplication/Main3Activity.kt
@@ -2,9 +2,7 @@ package com.example.kameko.myapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main3.*
 
@@ -14,18 +12,25 @@ class Main3Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
-        val items = Array(20, { i -> "Title-数は$i" })
+        val items = List<Map<String, String>>(20, { i -> mapOf("title" to "title-$i", "detail" to "detail-$i")})
+//        val items = Array(20, { i -> "Title-数は$i" })
 
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
-        //下も可能
-        val listView = findViewById(R.id.myListView1) as ListView
-        listView.adapter = adapter
+        val adapter = SimpleAdapter(
+                this,
+                items,
+                android.R.layout.simple_list_item_2,
+                arrayOf("title", "detail"),
+                intArrayOf(android.R.id.text1, android.R.id.text2)
+        )
+        myListView1.adapter = adapter
 
-//        myListView1.adapter = adapter
 
         myListView1.setOnItemClickListener { adapterView, view, position, id ->
-            val textView = view.findViewById<TextView>(android.R.id.text1)
-            Toast.makeText(this, "Clicked: ${textView.text}", Toast.LENGTH_SHORT).show()
+//            val textView = view.findViewById<TextView>(android.R.id.text1)
+            Toast.makeText(this, "トースト", Toast.LENGTH_SHORT).show()
+
+
+
         }
 
     }
